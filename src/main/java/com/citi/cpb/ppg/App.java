799d26a2c8d3cdf8aad291d04ee33b11537ff8e7
Adinath Shirsath <adinathshirsath@AdinathShirsath-Mac.local>
2018-08-22 15:07:01 -0400

    dependencies

---
+++ src/main/java/com/citi/cpb/ppg/App.java
@@ -0,0 +1,12 @@
+package com.citi.cpb.ppg;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
